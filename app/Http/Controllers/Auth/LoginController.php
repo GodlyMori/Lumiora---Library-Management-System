661a95a67f396e399ff9f8a27531b27f19d82b55
Alvin Legarbes <a.legarbes.554713@umindanao.edu.ph>
2026-05-21 20:51:17 +0800

    FIX

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -95,13 +95,13 @@ class LoginController extends Controller
 
         // DEBUG: uncomment if needed
         
-        // dd([
-        //     'now' => now(),
-        //     'expires_at' => $loginCode->expires_at,
-        //     'diff' => now()->diffInSeconds($loginCode->expires_at, false),
-        //     'code_db' => $loginCode->code,
-        //     'code_input' => $request->code,
-        // ]);
+        dd([
+            'now' => now(),
+            'expires_at' => $loginCode->expires_at,
+            'diff' => now()->diffInSeconds($loginCode->expires_at, false),
+            'code_db' => $loginCode->code,
+            'code_input' => $request->code,
+        ]);
         
 
         // SAFE expiration check using model cast helper
